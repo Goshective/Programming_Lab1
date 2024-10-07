7ec072e63291412bcec5a184b43af531f6f330e0
Goshective <den,skvortsoff@gmail.com>
2024-10-07 18:54:55 +0300

    Add find_empty_positions function

--- tests/lab3/test_sudoku.py
+++ tests/lab3/test_sudoku.py
@@ -6,7 +6,14 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 src_dir = os.path.join(current_dir, '..', '..')
 sys.path.insert(0, src_dir)
 
-from src.lab3.sudoku import read_sudoku, group, get_row, get_col, get_block
+from src.lab3.sudoku import (
+    read_sudoku, 
+    group, 
+    get_row, 
+    get_col, 
+    get_block, 
+    find_empty_positions,
+)
 
 
 class SudokuTestCase(unittest.TestCase):
@@ -32,6 +39,11 @@ class SudokuTestCase(unittest.TestCase):
         self.assertEqual(get_block(grid, (0, 1)), ['5', '3', '.', '6', '.', '.', '.', '9', '8'])
         self.assertEqual(get_block(grid, (4, 7)), ['.', '.', '3', '.', '.', '1', '.', '.', '6'])
         self.assertEqual(get_block(grid, (8, 8)), ['2', '8', '.', '.', '.', '5', '.', '7', '9'])
+
+    def test_find_empty(self):
+        self.assertEqual(find_empty_positions([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']]), (0, 2))
+        self.assertEqual(find_empty_positions([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']]), (1, 1))
+        self.assertEqual(find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']]), (2, 0))
 
 if __name__ == "__main__":
     unittest.main()
