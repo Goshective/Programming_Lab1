0584201b9d95ca85c95a4c067663ab8b8eaf323d
goshective <den.skvortsoff@gmail.com>
2024-11-29 18:17:15 +0300

    Fix bug in test names

--- tests/lab3/test_recommendations.py
+++ tests/lab3/test_recommendations.py
@@ -29,14 +29,14 @@ class RecommendationsTestCase(unittest.TestCase):
         self.assertEqual(user1.are_views_in_common(user3)[2], {2, 3})
         self.assertEqual(user3.are_views_in_common(user1)[2], {2, 3})
 
-    def test_user_get_recommendation(self):
+    def test_user_get_difference(self):
         user1 = User(1, [0, 1, 2, 2, 3])
         user2 = User(2, [4, 5, 5, 6])
         user3 = User(3, [2, 3, 4, 5, 6])
 
-        self.assertEqual(user2.get_recommendation(user3.unique_views), set([]))
-        self.assertEqual(user1.get_recommendation(user3.unique_views), {0, 1})
-        self.assertEqual(user3.get_recommendation(user1.unique_views), {4, 5, 6})
+        self.assertEqual(user2.get_difference(user3.unique_views), set([]))
+        self.assertEqual(user1.get_difference(user3.unique_views), {0, 1})
+        self.assertEqual(user3.get_difference(user1.unique_views), {4, 5, 6})
 
 
 if __name__ == "__main__":
